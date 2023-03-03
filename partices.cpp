579a2f0e560de29be76f56f3ca29ebc9219627d8
Safkat khan Tafsir <106735362+Safkat-Khan@users.noreply.github.com>
2023-03-04 02:07:37 +0600

    Add files via upload
    Last one

--- partices.cpp
+++ partices.cpp
@@ -1,85 +1,105 @@
 /*
-#include<iostream>
-#include<cstring>
+//<----------Clock Math---------->
+
+#include <bits/stdc++.h>
+
 using namespace std;
 
-int main()
-{
+int main() {
+	int H, M;
+	cin >> H >> M;
+	double deg=0.5*(60*H-11*M);
+	if(deg>180){
+		deg=360-deg;
+	}
+	cout << fixed << setprecision(7) << deg;
 
-string s;
-
-cin>>s;
-
-reverse(s.begin(),s.end());
-cout<<s;
+	return 0;
+}
 
 
+*/
 
-    return 0 ;
+
+/*
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+int a;
+cin>>a;
+if(a%)
+
+
+
+
+return 0;
 }
 */
 
 /*
-#include <algorithm>
-#include<iostream>
-#include<string>
+#include <iostream>
+#include <string>
 using namespace std;
+
 int main()
 {
-    string str = "Journal Dev reverse example";
-    reverse(str.begin(), str.end());
-	cout<<"\n"<<str;
-    return 0;
+ int t;
+ cin>>t;
+
+while(t--)
+ {
+     int X,P,Q,fine;
+   cin>>X>>P>>Q;
+   int tp=P-Q;
+    fine=X*tp;
+    cout<<fine<<endl;
+  }
+
+ return 0;
 }
 */
 
-/*
-#include<algorithm>
-#include<iostream>
-#include<cstring>
 
+/*
+//<------------------Devision---------------------->
+#include<bits/stdc++.h>
 using namespace std;
 
 int main(){
+    int N;
+    cin>>N;
+    for(int i=1;i<=N;i++){
+        if(N%i==0){
+            cout<<i<<endl;
+        }
+    }
 
-string s;
 
-cin>>s;
-string p=s;
-reverse(s.begin(),s.end());
 
-if(s==p){
-    cout<<"yes"<<endl;
+
+return 0;
+}
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+string A,B;
+int c,d;
+
+cin>>A>>B;
+c=A;
+d=B;
+if(A==B){
+    cout<<"Yes"<<endl;
 }
 else{
-    cout<<"no"<<endl;
+    cout<<"No"<<endl;
 }
 
-return 0;
 
-
-}
-*/
-
-
-#include<iostream>
-#include<math.h>
-
-using namespace std;
-int main(){
-
-double H,M,deg;
-cin>>H>>M;
-cout<<H<<" "<<M<<endl;
-deg=0.5*(11*M-60*H);
-cout<<"First Deg = "<<deg<<endl;
-if(deg<180){
-    deg=360-deg;
-}
-cout<<deg;
 
 
 return 0;
 }
-
-
